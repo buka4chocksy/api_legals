05c18d7244d5d936a537eb578d8372991423f998
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-03-25 18:02:17 +0100

    created index routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
-
-
-
-
-
-
+const authRoutes = require('./authRoutes');
+const practiceAreaRoutes = require('./practiceAreaRoutes')
+const jurisdictionRoutes = require('./jurisdictionRoutes')
 module.exports = (router) => {
+    router.use('/auth', authRoutes());
+    router.use('/practice_area', practiceAreaRoutes());
+    router.use('/jurisdiction',jurisdictionRoutes());
     return router;
 }
