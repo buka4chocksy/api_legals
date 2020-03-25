d320d31fc9831f9a22ba3a5c284e9fc887b466e1
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-03-25 17:06:51 +0100

    created server file

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var app = express();
-var morgan = require('morgan');
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyparser = require('body-parser')
 require('dotenv').config();
-var compression = require('compression');
-var router = express.Router();
-var rootRouter  = require('./src/routes/index.js')(router);
-var cors = require('cors');
-
-
+const compression = require('compression');
+const router = express.Router();
+const rootRouter  = require('./src/routes/index.js')(router);
+const cors = require('cors');
+const dbConfiguration = require('./bin/config/db');
 /**
  * Middlewares go here for the application.
  * if it gets to cumbersome then we can move to seperate file
@@ -20,11 +20,28 @@ var cors = require('cors');
 app.use(compression());
 app.use(morgan('dev'));
 app.use(express.static("public"));
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
 app.use(express.json());//for parsing application/json
 app.use(express.urlencoded({ extended: false})); //for parsing application/x-www-form-urlencoded
 app.use(cors());
 app.use('/api', rootRouter);
 
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+            error: {
+                    message: error.message
+            }
+    })
+})
+dbConfiguration()
 
 app.all('*', (req, res) => res.status(200).send({message : 'server is live'}));
 
