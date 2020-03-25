1fb0306187fc36fd2af796ff82e47ba9fd51c769
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-03-25 18:00:44 +0100

    created practicearea model

---
+++ src/models/practiceArea.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+const practice_area_schema = new schema({
+    name:{type:String , required:true}
+})
+module.exports = mongoose.model('practiceArea', practice_area_schema)
