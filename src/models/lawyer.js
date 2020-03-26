57a3ca221621f85d665f81052f79594847485409
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-03-27 00:23:43 +0100

    modified  lawyer model

--- src/models/lawyer.js
+++ src/models/lawyer.js
@@ -1,20 +1,21 @@
 const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 const lawyerSchema = new schema({
-user_id:{type: mongoose.SchemaTypes.ObjectId , ref:'user', autopopulate:true},
-enrollment_number:{type:String, default:''},
-practice_area:[{
-    practice_area_id:{type:mongoose.SchemaTypes.ObjectId, ref:'practiceArea', autopopulate:true}
-}],
-jurisdiction:[{
-    jurisdiction_id:{type:mongoose.SchemaTypes.ObjectId, ref:'jurisdiction', autopopulate:true}
-}],
-law_certificates:[{
-    image_url: { type: String , default:''},
-}],
-country:{type:String , default:''},
-state_of_origin:{type:String , default:''},
-gender:{type:String, default: ''},
+    user_id: { type: mongoose.SchemaTypes.ObjectId, ref: 'users', autopopulate: true },
+    public_id: { type: mongoose.SchemaTypes.ObjectId },
+    enrollment_number: { type: String, default: '' },
+    practice_area: [{
+        practice_area_id: { type: mongoose.SchemaTypes.ObjectId, ref: 'practiceArea', autopopulate: true }
+    }],
+    jurisdiction: [{
+        jurisdiction_id: { type: mongoose.SchemaTypes.ObjectId, ref: 'jurisdiction', autopopulate: true }
+    }],
+    law_certificates: [{
+        image_url: { type: String, default: '' },
+    }],
+    country: { type: String, default: '' },
+    state_of_origin: { type: String, default: '' },
+    gender: { type: String, default: '' }
 
 })
 
