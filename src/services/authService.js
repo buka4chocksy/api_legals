28be080a5ad05b1bae8c87ebcbbff05146953632
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-03-26 16:52:03 +0100

    modified registration and password change

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs')
 const mongoose = require('mongoose');
 const mailer = require('../middlewares/mailer');
 const secret = process.env.Secret
+const lawyer = require('../models/lawyer');
 const jwt = require('jsonwebtoken');
 
 //user signup
@@ -22,17 +23,46 @@ exports.Register = (data) => {
         }
         model.findOne({ email_address: userDetails.email_address }).then(found => {
             if (found) {
-                resolve({ success: false, message: 'User already exists!!!' })
+                //when a lawyer has created a user profile but didnt complete his signup
+                if(found.user_type == 'lawyer' ){
+                    if(found.status == false){
+                        getUserDetail(found.public_id).then(user =>{
+                            generateToken(user).then(token => {
+                                resolve({
+                                    success: true, data: token ,
+                                    message: 'please complete your signup process'
+                                })
+                            }).catch(err => reject(err))
+                        }).catch(err => reject(err))
+                    }else{
+                        resolve({ success: false, message: 'User already exists please proceed to sign in !!!' })
+                    }
+                }else{
+                    resolve({ success: false, message: 'User already exists!!!' })
+                }
             } else {
+                //
                 model.create(userDetails).then(created => {
                     if (created) {
-                        mailer.SignUpMail(userDetails.email_address, userDetails.status_code, userDetails.first_name, userDetails.last_name).then(sent => {
-                            if (sent) {
-                                resolve({ success: true, message: 'Registration successfull , proceed to verify ur account !!' })
-                            } else {
-                                resolve({ success: false, message: 'Error encountered while signing up !!' })
-                            }
-                        }).catch(err => reject(err));
+                        if (userDetails.user_type == 'lawyer') {
+                            const lawyer_id = created.public_id 
+                                    getUserDetail(lawyer_id).then(user =>{
+                                        generateToken(user).then(token => {
+                                            resolve({
+                                                success: true, data: token ,
+                                                message: 'proceed to fill your  practicearea , enrollment number and jurisdiction '
+                                            })
+                                        }).catch(err => reject(err))
+                                    }).catch(err => reject(err))
+                        } else {
+                            mailer.SignUpMail(userDetails.email_address, userDetails.status_code, userDetails.first_name, userDetails.last_name).then(sent => {
+                                if (sent) {
+                                    resolve({ success: true, message: 'Registration successfull , proceed to verify ur account !!' })
+                                } else {
+                                    resolve({ success: false, message: 'Error encountered while signing up !!' })
+                                }
+                            }).catch(err => reject(err));
+                        }
                     } else {
                         resolve({ success: false, message: 'Error registering user !!' })
                     }
@@ -79,56 +109,57 @@ exports.verifyUser = option => {
 };
 
 //user login
-exports.userLogin = (email_address , password)=>{
-    return new Promise((resolve , reject)=>{
-        model.findOne({email_address:email_address}, { _id: 0, __v: 0  , }).then(user =>{
-            if(user){
-                if(user.status != true)reject({success:false , message:'account not veririfed !!!'})
-                const comparePassword = bcrypt.compareSync(password , user.password)
-                if(comparePassword){
-                    getUserDetail(user.publicId).then(activeUser =>{
-                        generateToken(activeUser).then(token =>{
-                            resolve({success:true , data:{activeUser , token:token },
-                            message:'authentication successfull !!!'
+exports.userLogin = (email_address, password) => {
+    return new Promise((resolve, reject) => {
+        model.findOne({ email_address: email_address }, { _id: 0, __v: 0, }).then(user => {
+            if (user) {
+                if (user.status != true) reject({ success: false, message: 'account not verified !!!' })
+                const comparePassword = bcrypt.compareSync(password, user.password)
+                if (comparePassword) {
+                    getUserDetail(user.publicId).then(activeUser => {
+                        generateToken(activeUser).then(token => {
+                            resolve({
+                                success: true, data: { activeUser, token: token },
+                                message: 'authentication successfull !!!'
                             })
                         }).catch(err => reject(err))
                     }).catch(err => reject(err))
-                }else{
-                    resolve({success:false , message:'incorrect email or password '})
+                } else {
+                    resolve({ success: false, message: 'incorrect email or password ' })
                 }
-            }else{
-                resolve({success:false , message:'user does not exist !!!'})  
+            } else {
+                resolve({ success: false, message: 'user does not exist !!!' })
             }
-        }).catch(err =>{
+        }).catch(err => {
             reject(err)
         })
     })
 }
 
 //user password change
-exports.changePassword = (email , data)=>{
-    return new Promise((resolve , reject)=>{
-        model.findOne({email_address:email}).then(exists =>{
-            if(exists){
+exports.changePassword = (email, data) => {
+    return new Promise((resolve, reject) => {
+        model.findOne({ email_address: email }).then(exists => {
+            if (exists) {
                 const db_password = exists.password
                 const old_password = data.password
-                const new_password = bcrypt.hashSync(data.comfirm_password ,10)
+                const new_password = bcrypt.hashSync(data.comfirm_password, 10)
 
-                const compare_password = bcrypt.compareSync(old_password ,db_password)
-                if(compare_password == true){
-                    model.findOneAndUpdate({email_address:email},{password: new_password})
-                    .then(updated =>{
-                        if(updated){
-                            resolve({success:true , message:'Password updated successfully !!'})
-                        }else{
-                            resolve({success:false , message:'Password was not updated successfully !!'})
-                        }
-                    }).catch(err => reject(err))
-                }else{
-                    resolve({success:false , message:'Invalid password inserted !!!'})
+                const compare_password = bcrypt.compareSync(old_password, db_password)
+                if (compare_password == true) {
+                    model.findOneAndUpdate({ email_address: email }, { password: new_password })
+                        .then(updated => {
+                            if (updated) {
+                                resolve({ success: true, message: 'Password updated successfully !!' })
+                            } else {
+                                resolve({ success: false, message: 'Password was not updated successfully !!' })
+                            }
+                        }).catch(err => reject(err))
+                } else {
+                    resolve({ success: false, message: 'Invalid password inserted !!!' })
                 }
-            }else{
-                resolve({success:false , message:'user does not exists !!!'})
+            } else {
+                resolve({ success: false, message: 'user does not exists !!!' })
             }
         }).catch(err => reject(err));
     })
@@ -138,52 +169,53 @@ exports.changePassword = (email , data)=>{
 function getUserDetail(Id) {
     return new Promise((resolve, reject) => {
 
-      model.findOne({publicId: Id }, { _id: 0, __v: 0 })
-        .then(data => {
-          var specificUserDetail = {
-            email_address: data.email_address,
-            fullname: data.first_name +' '+ data.last_name, 
-            phone: data.phone_number,
-            publicId: data.public_id,
-            userType: data.user_type,
-            status:data.status
-          };
-          resolve(specificUserDetail);
-        })
-        .catch(error => reject(error));
+        model.findOne({ public_id: Id }, { _id: 0, __v: 0 })
+            .then(data => {
+                var specificUserDetail = {
+                    email_address: data.email_address,
+                    fullname: data.first_name + ' ' + data.last_name,
+                    phone: data.phone_number,
+                    publicId: data.public_id,
+                    userType: data.user_type,
+                    status: data.status,
+                    image_url:data.image_url
+                };
+                resolve(specificUserDetail);
+            })
+            .catch(error => reject(error));
     });
-  }
-  exports.getUserDetail = getUserDetail
+}
+exports.getUserDetail = getUserDetail
 
-   //generate token
-   function generateToken(data = {}) {
+//generate token
+function generateToken(data = {}) {
     return new Promise((resolve, reject) => {
-      jwt.sign({ ...data }, secret, { expiresIn: "24hrs" }, function(err, token) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(token);
-        }
-      });
+        jwt.sign({ ...data }, secret, { expiresIn: "24hrs" }, function (err, token) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(token);
+            }
+        });
     });
-  }
-  
-  exports.generateToken = generateToken;
+}
 
-    //verify user token
-    function verifyToken(token = "") {
-        return new Promise((resolve, reject) => {
-          jwt.verify(token.replace("Bearer", ""), secret, function(
+exports.generateToken = generateToken;
+
+//verify user token
+function verifyToken(token = "") {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token.replace("Bearer", ""), secret, function (
             err,
             decodedToken
-          ) {
+        ) {
             if (err) {
-              reject(err);
+                reject(err);
             } else {
-              resolve(decodedToken);
+                resolve(decodedToken);
             }
-          });
         });
-      }
-    
-      exports.verifyToken = verifyToken
+    });
+}
+
+exports.verifyToken = verifyToken
