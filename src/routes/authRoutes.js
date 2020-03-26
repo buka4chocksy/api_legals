ed6277103cbe962d7dea95004ce63771f80ca2b9
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-03-27 00:25:00 +0100

    added verify , authenticate ,change password and profile picture upload routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,14 @@
 const router = require('express').Router()
 const authController = require('../controllers/authController')
 const middleware = require('../middlewares/authMiddleware');
-module.exports = function(){
+const multer = require('../middlewares/multer');
+module.exports = function () {
     const authCtrl = new authController()
     router.post('/register', authCtrl.Register)
-    router.put('/verify',authCtrl.verifyUser)
+    router.put('/verify', authCtrl.verifyUser)
     router.post('/authenticate', authCtrl.loginUser)
-    router.put('/change_password',middleware.authenticate , authCtrl.changePassword)
+    router.put('/change_password', middleware.authenticate, authCtrl.changePassword)
+    router.put('/profile_picture', middleware.authenticate ,multer.upload.single('profile'), authCtrl.uploadProfilePicture )
+
     return router;
 }
