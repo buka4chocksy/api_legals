9aae55ca2611e1c0302b94aac39929bb7244178a
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-03-27 00:26:02 +0100

    added get profile , update profile and delete account routes

--- src/routes/lawyerRoutes.js
+++ src/routes/lawyerRoutes.js
@@ -5,6 +5,10 @@ const multer = require('../middlewares/multer');
 module.exports = function(){
     const lawCtrl = new lawyerController()
     router.put('/reg', middleware.authenticate, lawCtrl.completeLawyerRegistration)
-    router.put('/update', middleware.authenticate, multer.upload.single('certificate'), lawCtrl.uploadCertificate)
+    router.put('/upload_cert', middleware.authenticate, multer.upload.single('certificate'), lawCtrl.uploadCertificate)
+    router.get('/profile', middleware.authenticate, lawCtrl.getLawyerProile)
+    router.post('/update_profile', middleware.authenticate, lawCtrl.updateLawyerProfile)
+    router.delete('/delete_account', middleware.authenticate, lawCtrl.deleteUserAccount)
+
     return router;
 }
