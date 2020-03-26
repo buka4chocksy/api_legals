6d0104c5a88335738fcf8c56d705ab47f14d7102
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-03-27 00:21:53 +0100

    modified change password and added profile picture upload

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,5 @@
 const service = require('../services/authService')
-
+const cloudinary = require('../middlewares/cloudinary');
 module.exports = function authController() {
 
     this.Register = (req, res, next) => {
@@ -20,13 +20,37 @@ module.exports = function authController() {
         }).catch(err => res.status(500).send(err));
     }
 
-    this.changePassword = (req, res)=>{
+    this.changePassword = (req, res) => {
         const data = {
-            password:req.body.password,
-            comfirm_password:req.body.comfirm_password
+            password: req.body.password,
+            comfirm_password: req.body.new_password
         }
-        service.changePassword(req.auth.email ,data ).then(data =>{
+        service.changePassword(req.auth.publicId, data).then(data => {
             res.status(200).send(data)
         }).catch(err => res.status(500).send(err));
     }
+
+
+    this.uploadProfilePicture = async (req, res) => {
+        var requestDetails = {
+            image: req.file != null && req.file !== undefined ? req.file.path : null
+        };
+
+        if (req.image !== null && req.file !== undefined) {
+            await cloudinary.uploadToCloud(requestDetails.image).then(img => {
+                requestDetails.imageUrl = img.url;
+                requestDetails.imageID = img.ID;
+                return requestDetails;
+            });
+        }
+        service
+            .profilePicture(req.auth.publicId, requestDetails)
+            .then(data => {
+                res.status(200).send(data);
+            })
+            .catch(err => {
+                res.status(500).send(err);
+            });
+    };
+
 }
