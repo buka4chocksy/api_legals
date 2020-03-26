ad6c8703fc823a2c44e0ebc90f904bfc01886151
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-03-26 16:50:01 +0100

    created lawyer index routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const authRoutes = require('./authRoutes');
 const practiceAreaRoutes = require('./practiceAreaRoutes')
 const jurisdictionRoutes = require('./jurisdictionRoutes')
+const lawyerRoutes = require('./lawyerRoutes')
 module.exports = (router) => {
     router.use('/auth', authRoutes());
     router.use('/practice_area', practiceAreaRoutes());
     router.use('/jurisdiction',jurisdictionRoutes());
+    router.use('/lawyer', lawyerRoutes())
     return router;
 }
