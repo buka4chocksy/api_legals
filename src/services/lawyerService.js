fbe3c989b1aa4f54a930e3e7dccab6118d3ad03d
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-03-27 21:58:02 +0100

    modified

--- src/services/lawyerService.js
+++ src/services/lawyerService.js
@@ -57,8 +57,8 @@ exports.uploadCertificate = (id, publicId, data) => {
                             resolve({ success: false, message: 'Error encountered while completing lawyer signup' })
                         }
                     })
-                }else{
-                    resolve({ success: false, message: 'Error encountered while completing lawyer signup' }) 
+                } else {
+                    resolve({ success: false, message: 'Error encountered while completing lawyer signup' })
                 }
             })
     })
@@ -103,21 +103,86 @@ exports.editLawyerProfile = (id, data) => {
 //delete user account
 exports.deleteAccount = (id) => {
     return new Promise((resolve, reject) => {
-        model.findOneAndRemove({user_id:id}).exec((err , deleted)=>{
-            if(err)reject(err);
-            if(deleted){
-                user.findByIdAndDelete({_id:id}).exec((err , done)=>{
-                    if(err)reject(err);
-                    if(done){
-                        resolve({success:true , message:'account deleted'})
-                    }else{
-                        resolve({success:false  , message:'error deleting account !!!'})
+        model.findOneAndRemove({ user_id: id }).exec((err, deleted) => {
+            if (err) reject(err);
+            if (deleted) {
+                user.findByIdAndDelete({ _id: id }).exec((err, done) => {
+                    if (err) reject(err);
+                    if (done) {
+                        resolve({ success: true, message: 'account deleted' })
+                    } else {
+                        resolve({ success: false, message: 'error deleting account !!!' })
                     }
                 })
-            }else{
-                resolve({success:false  , message:'error deleting account !!!'})
+            } else {
+                resolve({ success: false, message: 'error deleting account !!!' })
 
             }
         })
+    })
+}
+
+//get list of lawyer 
+
+exports.lawyerList = (pagenumber = 1, pagesize = 20,) => {
+    return new Promise((resolve, reject) => {
+        model.find({}).skip((parseInt(pagenumber - 1) * parseInt(pagesize))).limit(parseInt(pagesize))
+        .populate({ path: 'user_id', model: 'users' })
+            .populate({ path: "practice_area.practice_area_id", model: 'practiceArea', select: { _id: 0, __v: 0 } })
+            .populate({ path: "jurisdiction.jurisdiction_id", model: 'jurisdiction', select: { _id: 0, __v: 0 } })
+            .exec((err, found) => {
+                if (err) reject(err);
+                if (found) {
+                    resolve({ success: true, message: 'lawyers found', data: found })
+                } else {
+                    resolve({ success: false, message: 'could not find list of lawyers' })
+                }
+            })
+    })
+}
+
+//sort lawyer by practice area
+exports.sortLawerpracticeArea = (id, pagenumber = 1, pagesize = 20) => {
+    return new Promise((resolve, reject) => {
+        model.find({ 'practice_area.practice_area_id': id })
+            .skip((parseInt(pagenumber - 1) * parseInt(pagesize))).limit(parseInt(pagesize))
+            .populate({ path: 'user_id', model: 'users' })
+            .populate({ path: "practice_area.practice_area_id", model: 'practiceArea', select: { _id: 0, __v: 0 } })
+            .populate({ path: "jurisdiction.jurisdiction_id", model: 'jurisdiction', select: { _id: 0, __v: 0 } })
+            .exec((err, found) => {
+                if (err) reject(err);
+                if (found) {
+                    var maps = found.sort(function (a, b) {
+                        return b.user_id.first_name.length - a.user_id.first_name.length;
+
+                    })
+                    resolve({ success: true, message: 'lawfirms found', data: maps })
+                } else {
+                    resolve({ success: false, message: 'Could  not find data' })
+                }
+            })
+    })
+}
+
+//sort lawyer by location
+exports.sortLawyeryLocation = (data, pagenumber = 1, pagesize = 20) => {
+    return new Promise((resolve, reject) => {
+        model.find({ 'country': data.country , 'state_of_origin':data.state })
+            .skip((parseInt(pagenumber - 1) * parseInt(pagesize))).limit(parseInt(pagesize))
+            .populate({ path: 'user_id', model: 'users' })
+            .populate({ path: "practice_area.practice_area_id", model: 'practiceArea', select: { _id: 0, __v: 0 } })
+            .populate({ path: "jurisdiction.jurisdiction_id", model: 'jurisdiction', select: { _id: 0, __v: 0 } })
+            .exec((err, found) => {
+                if (err) reject(err);
+                if (found) {
+                    var maps = found.sort(function (a, b) {
+                        return b.user_id.first_name.length - a.user_id.first_name.length;
+
+                    })
+                    resolve({ success: true, message: 'lawfirms found', data: maps })
+                } else {
+                    resolve({ success: false, message: 'Could  not find data' })
+                }
+            })
     })
 }
