8fea3d8334782953c0a38a52b966a6e78a07fe40
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-03-27 21:56:29 +0100

    created lawfirm routes

---
+++ src/routes/lawFirmRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const lawFirmController = require('../controllers/lawFirmController')
+const middleware = require('../middlewares/authMiddleware');
+const multer = require('../middlewares/multer');
+module.exports = function () {
+    const lawFirmCtrl = new lawFirmController()
+    router.post('/create', middleware.authenticate, lawFirmCtrl.createLawFirm);
+    router.put('/profile_picture', middleware.authenticate, multer.upload.single('profile'), lawFirmCtrl.uploadProfilePicture)
+    router.get('/profile', middleware.authenticate , lawFirmCtrl.getlawfirmProfile)
+    router.get('/lawfirms/:pagesize/:pagenumber', middleware.authenticate , lawFirmCtrl.getLawfirmList)
+    router.get('/sort_practice_area/:id/:pagesize/:pagenumber', middleware.authenticate , lawFirmCtrl.sortLawFirmByPracticeArea)
+    router.get('/sort_location/:pagesize/:pagenumber', middleware.authenticate , lawFirmCtrl.sortLawFirmByLocation)
+    return router;
+}
