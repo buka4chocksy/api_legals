e21f41aba8f0524e65e32162e647bf9ea5cbd94f
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-03-31 11:36:06 +0100

    added search lawyer and profile picture update methods

--- src/services/lawyerService.js
+++ src/services/lawyerService.js
@@ -2,43 +2,56 @@ const model = require('../models/lawyer');
 const user = require('../models/users')
 const authService = require('../services/authService')
 
-exports.completelawyerRegisteration = (id, publicId, data) => {
+exports.completelawyerRegisteration = ( publicId, data) => {
     return new Promise((resolve, reject) => {
-        const details = {
-            user_id: id,
-            public_id: publicId,
-            enrollment_number: data.enrollment_number,
-            practice_area: [{
-                practice_area_id: data.practice_area
-            }],
-            jurisdiction: [{
-                jurisdiction_id: data.jurisdiction
-            }]
-        }
-        model.findOne({ user_id: id }).exec((err, exists) => {
-            if (err) reject(err)
-            if (exists) {
-                resolve({ success: true, message: 'lawyer details already exists , please proceed to upload certificate' })
-            } else {
-                model.create(details).then(created => {
-                    if (created) {
-                        resolve({ success: true, message: 'complete your registration by uploading your certificate' })
+        user.findOne({public_id:publicId}).exec((err , got)=>{
+            if(err)reject(err)
+            if(got){
+                const details = {
+                    first_name:got.first_name,
+                    last_name:got.last_name,
+                    email_address:got.email_address,
+                    phone_number:got.phone_number,
+                    user_type:got.user_type,
+                    public_id: publicId,
+                    enrollment_number: data.enrollment_number,
+                    practice_area: [{
+                    practice_area_id: data.practice_area
+                    }],
+                    jurisdiction: [{
+                        jurisdiction_id: data.jurisdiction
+                    }]
+                }
+                model.findOne({ public_id: publicId }).exec((err, exists) => {
+                    if (err) reject(err)
+                    if (exists) {
+                        resolve({ success: true, message: 'lawyer details already exists , please proceed to upload certificate' })
                     } else {
-                        resolve({ success: true, message: 'Error encountered while adding lawyer details' })
+                        model.create(details).then(created => {
+                            if (created) {
+                               resolve({ success: true, message: 'complete your registration by uploading your certificate' })
+                            } else {
+                                resolve({ success: true, message: 'Error encountered while adding lawyer details' })
+                            }
+                        }).catch(err => reject(err))
                     }
-                }).catch(err => reject(err))
+                })
+
+            }else{
+                resolve({ success: true, message: 'lawyer does not exist' })
             }
         })
+
 
     })
 }
 
-exports.uploadCertificate = (id, publicId, data) => {
+exports.uploadCertificate = (publicId, data) => {
     return new Promise((resolve, reject) => {
         const detail = {
             image_url: data.imageUrl
         }
-        model.findOneAndUpdate({ user_id: id }, { $push: { law_certificates: detail } })
+        model.findOneAndUpdate({ public_id: publicId }, { $push: { law_certificates: detail } })
             .exec((err, updated) => {
                 if (err) reject(err);
                 if (updated) {
@@ -67,7 +80,7 @@ exports.uploadCertificate = (id, publicId, data) => {
 //get lawyer profile 
 exports.getLawyerProfile = (id) => {
     return new Promise((resolve, reject) => {
-        model.find({ user_id: id }).populate({ path: 'user_id', model: 'users' })
+        model.find({ public_id: id })
             .populate({ path: "practice_area.practice_area_id", model: 'practiceArea', select: { _id: 0, __v: 0 } })
             .populate({ path: "jurisdiction.jurisdiction_id", model: 'jurisdiction', select: { _id: 0, __v: 0 } })
             .exec((err, found) => {
@@ -101,9 +114,9 @@ exports.editLawyerProfile = (id, data) => {
 }
 
 //delete user account
-exports.deleteAccount = (id) => {
+exports.deleteAccount = (id , publicId) => {
     return new Promise((resolve, reject) => {
-        model.findOneAndRemove({ user_id: id }).exec((err, deleted) => {
+        model.findOneAndRemove({ public_id: publicId }).exec((err, deleted) => {
             if (err) reject(err);
             if (deleted) {
                 user.findByIdAndDelete({ _id: id }).exec((err, done) => {
@@ -124,10 +137,9 @@ exports.deleteAccount = (id) => {
 
 //get list of lawyer 
 
-exports.lawyerList = (pagenumber = 1, pagesize = 20,) => {
+exports.lawyerList = (pagenumber = 1, pagesize = 20, ) => {
     return new Promise((resolve, reject) => {
         model.find({}).skip((parseInt(pagenumber - 1) * parseInt(pagesize))).limit(parseInt(pagesize))
-        .populate({ path: 'user_id', model: 'users' })
             .populate({ path: "practice_area.practice_area_id", model: 'practiceArea', select: { _id: 0, __v: 0 } })
             .populate({ path: "jurisdiction.jurisdiction_id", model: 'jurisdiction', select: { _id: 0, __v: 0 } })
             .exec((err, found) => {
@@ -146,17 +158,16 @@ exports.sortLawerpracticeArea = (id, pagenumber = 1, pagesize = 20) => {
     return new Promise((resolve, reject) => {
         model.find({ 'practice_area.practice_area_id': id })
             .skip((parseInt(pagenumber - 1) * parseInt(pagesize))).limit(parseInt(pagesize))
-            .populate({ path: 'user_id', model: 'users' })
             .populate({ path: "practice_area.practice_area_id", model: 'practiceArea', select: { _id: 0, __v: 0 } })
             .populate({ path: "jurisdiction.jurisdiction_id", model: 'jurisdiction', select: { _id: 0, __v: 0 } })
             .exec((err, found) => {
                 if (err) reject(err);
                 if (found) {
                     var maps = found.sort(function (a, b) {
-                        return b.user_id.first_name.length - a.user_id.first_name.length;
+                        return b.first_name.length - a.first_name.length;
 
                     })
-                    resolve({ success: true, message: 'lawfirms found', data: maps })
+                    resolve({ success: true, message: 'lawyers found', data: maps })
                 } else {
                     resolve({ success: false, message: 'Could  not find data' })
                 }
@@ -167,22 +178,62 @@ exports.sortLawerpracticeArea = (id, pagenumber = 1, pagesize = 20) => {
 //sort lawyer by location
 exports.sortLawyeryLocation = (data, pagenumber = 1, pagesize = 20) => {
     return new Promise((resolve, reject) => {
-        model.find({ 'country': data.country , 'state_of_origin':data.state })
+        model.find({ 'country': data.country, 'state_of_origin': data.state })
             .skip((parseInt(pagenumber - 1) * parseInt(pagesize))).limit(parseInt(pagesize))
-            .populate({ path: 'user_id', model: 'users' })
             .populate({ path: "practice_area.practice_area_id", model: 'practiceArea', select: { _id: 0, __v: 0 } })
             .populate({ path: "jurisdiction.jurisdiction_id", model: 'jurisdiction', select: { _id: 0, __v: 0 } })
             .exec((err, found) => {
                 if (err) reject(err);
                 if (found) {
                     var maps = found.sort(function (a, b) {
-                        return b.user_id.first_name.length - a.user_id.first_name.length;
+                        return b.first_name.length - a.first_name.length;
 
                     })
-                    resolve({ success: true, message: 'lawfirms found', data: maps })
+                    resolve({ success: true, message: 'lawyers found', data: maps })
                 } else {
                     resolve({ success: false, message: 'Could  not find data' })
                 }
             })
+    })
+}
+
+//search for lawyer
+exports.searchLawyer = function (option) {
+    return new Promise((resolve, reject) => {
+        model.find({ $or: [{ first_name: { $regex: option.search, $options: 'i' } }, { last_name: { $regex: option.search, $options: 'i' } }, 
+        { email_address: { $regex: option.search, $options: 'i' } }] }, { _id: 0, __v: 0  , password:0 , status_code:0})
+        .populate({ path: "practice_area.practice_area_id", model: 'practiceArea', select: { _id: 0, __v: 0 } })
+        .populate({ path: "jurisdiction.jurisdiction_id", model: 'jurisdiction', select: { _id: 0, __v: 0 } })
+        .exec((err, found) => {
+
+                if (err) { reject(err); }
+                if (found == null || Object.keys(found).length === 0) {
+                    resolve({ success: false, data: {}, message: "We could not find what you are looking for." })
+                } else {
+                    var maps = found.sort(function (a, b) {
+                        return b.first_name.length - a.first_name.length;
+
+                    })
+                    resolve({ success: true, data: maps, message: "" });
+                }
+            })
+    })
+}
+
+//profile picture update
+exports.profilePicture = (id, data) => {
+    return new Promise((resolve, reject) => {
+        const detail = {
+            image_url: data.imageUrl,
+            image_id: data.imageID
+        }
+        model.findOneAndUpdate({ public_id: id }, detail).exec((err, updated) => {
+            if (err) reject(err);
+            if (updated) {
+                resolve({ success: true, message: 'profile picture updated ' })
+            } else {
+                resolve({ success: false, message: 'Error updating profile picture' })
+            }
+        })
     })
 }
