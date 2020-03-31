21b866ae817a683cbfd1c91be8b4d5c2688d9588
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-03-31 11:31:06 +0100

    added client index routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const practiceAreaRoutes = require('./practiceAreaRoutes')
 const jurisdictionRoutes = require('./jurisdictionRoutes')
 const lawyerRoutes = require('./lawyerRoutes')
 const lawFirmRoutes = require('./lawFirmRoutes')
+const clientRoutes = require('./clientRoutes')
 module.exports = (router) => {
     router.use('/auth', authRoutes());
     router.use('/practice_area', practiceAreaRoutes());
     router.use('/jurisdiction', jurisdictionRoutes());
     router.use('/lawyer', lawyerRoutes())
     router.use('/lawfirm', lawFirmRoutes())
+    router.use('/client', clientRoutes())
     return router;
 }
