908add47aa7370c91751e787946a53801e88a4df
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-03-31 11:31:25 +0100

    added lawfirm search routes

--- src/routes/lawFirmRoutes.js
+++ src/routes/lawFirmRoutes.js
@@ -10,5 +10,7 @@ module.exports = function () {
     router.get('/lawfirms/:pagesize/:pagenumber', middleware.authenticate , lawFirmCtrl.getLawfirmList)
     router.get('/sort_practice_area/:id/:pagesize/:pagenumber', middleware.authenticate , lawFirmCtrl.sortLawFirmByPracticeArea)
     router.get('/sort_location/:pagesize/:pagenumber', middleware.authenticate , lawFirmCtrl.sortLawFirmByLocation)
+    router.get('/search', middleware.authenticate , lawFirmCtrl.searchLawFirm)
+
     return router;
 }
