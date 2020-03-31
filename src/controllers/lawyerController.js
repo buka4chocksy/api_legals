8daeee2c78cfa98880ef41fbc0e4c1be584ab68f
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-03-31 11:29:37 +0100

    added search lawyer and upload profile picture methods

--- src/controllers/lawyerController.js
+++ src/controllers/lawyerController.js
@@ -2,7 +2,7 @@ const service = require('../services/lawyerService')
 const cloudinary = require('../middlewares/cloudinary');
 module.exports = function lawyerController() {
     this.completeLawyerRegistration = (req, res) => {
-        service.completelawyerRegisteration(req.auth.Id, req.auth.publicId, req.body).then(data => {
+        service.completelawyerRegisteration(req.auth.publicId, req.body).then(data => {
             res.status(200).send(data)
         }).catch(err => res.status(500).send(err));
     }
@@ -20,7 +20,7 @@ module.exports = function lawyerController() {
             });
         }
         service
-            .uploadCertificate(req.auth.Id, req.auth.publicId, requestDetails)
+            .uploadCertificate(req.auth.publicId, requestDetails)
             .then(data => {
                 res.status(200).send(data);
             })
@@ -30,7 +30,7 @@ module.exports = function lawyerController() {
     };
 
     this.getLawyerProile = (req, res) => {
-        service.getLawyerProfile(req.auth.Id).then(data => {
+        service.getLawyerProfile(req.auth.publicId).then(data => {
             res.status(200).send(data);
         }).catch(err => res.status(500).send(err))
     }
@@ -42,7 +42,7 @@ module.exports = function lawyerController() {
     }
 
     this.deleteUserAccount = (req, res) => {
-        service.deleteAccount(req.auth.Id).then(data => {
+        service.deleteAccount(req.auth.Id , req.auth.publicId).then(data => {
             res.status(200).send(data);
         }).catch(err => res.status(500).send(err))
     }
@@ -66,5 +66,34 @@ module.exports = function lawyerController() {
                 res.status(200).send(data);
             }).catch(err => res.status(500).send(err))
     }
+
+    this.searchLawyer = (req, res)=>{
+        service.searchLawyer(req.body).then(data =>{
+            res.status(200).send(data);
+        }).catch(err => res.status(500).send(err))
+    }
+
+
+    this.uploadProfilePicture = async (req, res) => {
+        var requestDetails = {
+            image: req.file != null && req.file !== undefined ? req.file.path : null
+        };
+
+        if (req.image !== null && req.file !== undefined) {
+            await cloudinary.uploadToCloud(requestDetails.image).then(img => {
+                requestDetails.imageUrl = img.url;
+                requestDetails.imageID = img.ID;
+                return requestDetails;
+            });
+        }
+        service
+            .profilePicture(req.auth.publicId, requestDetails)
+            .then(data => {
+                res.status(200).send(data);
+            })
+            .catch(err => {
+                res.status(500).send(err);
+            });
+    };
 
 }
