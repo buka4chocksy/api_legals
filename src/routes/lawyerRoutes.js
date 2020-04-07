6e7a8d7d5fd1353853b49d5a11a2fbd2b93e7d1a
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-04-07 14:17:56 +0100

    modified lawyer routes

--- src/routes/lawyerRoutes.js
+++ src/routes/lawyerRoutes.js
@@ -4,8 +4,7 @@ const middleware = require('../middlewares/authMiddleware')
 const multer = require('../middlewares/multer');
 module.exports = function(){
     const lawCtrl = new lawyerController()
-    router.put('/reg', middleware.authenticate, lawCtrl.completeLawyerRegistration)
-    router.put('/upload_cert', middleware.authenticate, multer.upload.single('certificate'), lawCtrl.uploadCertificate)
+    router.put('/reg', middleware.authenticate, multer.upload.single('certificate'), lawCtrl.completeLawyerRegistration)
     router.get('/profile', middleware.authenticate, lawCtrl.getLawyerProile)
     router.post('/update_profile', middleware.authenticate, lawCtrl.updateLawyerProfile)
     router.delete('/delete_account', middleware.authenticate, lawCtrl.deleteUserAccount)
