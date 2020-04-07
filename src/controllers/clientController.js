0b36126ca05d38064829025ed25b010f8a8926f2
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-04-07 14:11:34 +0100

    created uploadProfilePicture , updateClientProfile, deleteUserAccount, updateClientProfile ,getClientDetails methods

---
+++ src/controllers/clientController.js
@@ -0,0 +1,43 @@
+const service = require('../services/clientService');
+const cloudinary = require('../middlewares/cloudinary')
+module.exports =  function clientController(){
+    this.uploadProfilePicture = async (req, res) => {
+        var requestDetails = {
+            image: req.file != null && req.file !== undefined ? req.file.path : null
+        };
+
+        if (req.image !== null && req.file !== undefined) {
+            await cloudinary.uploadToCloud(requestDetails.image).then(img => {
+                requestDetails.imageUrl = img.url;
+                requestDetails.imageID = img.ID;
+                return requestDetails;
+            });
+        }
+        service
+            .profilePicture(req.auth.publicId, requestDetails)
+            .then(data => {
+                res.status(200).send(data);
+            })
+            .catch(err => {
+                res.status(500).send(err);
+            });
+    };
+
+    this.updateClientProfile = (req, res) => {
+        service.editClientProfile(req.auth.publicId, req.body).then(data => {
+            res.status(200).send(data);
+        }).catch(err => res.status(500).send(err))
+    }
+
+    this.deleteUserAccount = (req, res) => {
+        service.deleteAccount(req.auth.Id , req.auth.publicId).then(data => {
+            res.status(200).send(data);
+        }).catch(err => res.status(500).send(err))
+    }
+
+    this.getClientDetails = (req , res)=>{
+        service.getClientProfile(req.auth.publicId).then(data =>{
+            res.status(200).send(data);
+        }).catch(err => res.status(500).send(err))
+    }
+}
