7c62036b03ffa103d3e0f13b356e9ae49b2c9e37
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-04-17 22:25:12 +0100

    created methods for GetClientPendingOpenMatter , getPendingSpecifiedMatter, sortClientMatterByPracticeArea

--- src/controllers/matterController.js
+++ src/controllers/matterController.js
@@ -17,4 +17,23 @@ module.exports =  function matterController(){
         res.status(200).send(data)
         }).catch(err => res.status(500).send(err));
     }
+
+    this.GetClientPendingOpenMatter = (req,res)=>{
+        service.GetClientPendingOpenMatter(req.auth.publicId , req.params.pagenumber, req.params.pagesize).then(data =>{
+            res.status(200).send(data)
+            }).catch(err => res.status(500).send(err));
+    }
+
+    this.getPendingSpecifiedMatter = (req,res)=>{
+        service.getClientPendingSpecifiedMatter(req.auth.publicId , req.params.pagenumber, req.params.pagesize).then(data =>{
+            res.status(200).send(data)
+            }).catch(err => res.status(500).send(err));
+    }
+
+    this.sortClientMatterByPracticeArea = (req,res)=>{
+        service.sortClientMatterByPracticeArea(req.auth.publicId , req.body.practiceArea ,req.params.pagenumber, req.params.pagesize).then(data =>{
+            res.status(200).send(data)
+            }).catch(err => res.status(500).send(err));
+    }
+    
 }
