dd75b373a2fa0b6b54ca791ee40a8c2652c37443
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-04-17 22:26:00 +0100

    added routes for GetClientPendingOpenMatter , getPendingSpecifiedMatter, sortClientMatterByPracticeArea

--- src/routes/matterRoutes.js
+++ src/routes/matterRoutes.js
@@ -5,6 +5,8 @@ const multer = require('../middlewares/multer');
 module.exports = function () {
     const matterCtrl = new matterController()
     router.put('/create', middleware.authenticate, multer.upload.single('image'), matterCtrl.createMatter);
-
+    router.get('/pending_open_matters/:pagesize/:pagenumber' , middleware.authenticate, matterCtrl.GetClientPendingOpenMatter);
+    router.get('/pending_specified_matters/:pagesize/:pagenumber' , middleware.authenticate, matterCtrl.getPendingSpecifiedMatter);
+    router.get('/sort_by_practicearea/:pagesize/:pagenumber', middleware.authenticate, matterCtrl.sortClientMatterByPracticeArea)
     return router;
 }
