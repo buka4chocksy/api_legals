113557e19e82f5a9932a3f1acf02fc3ee8fc9e16
Igwe Augustine <augustusbest00@gmail.com>
2020-04-23 17:32:18 +0100

    error fix

--- src/models/users.js
+++ src/models/users.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 const userSchema = new schema({
-    first_name:{type:String , required:true},
-    last_name:{type:String, required:true},
-    oauthID: {type: String},
-    name: {type: String},
-    email_address:{type:String , required:true , unique:true},
-    phone_number:{type:String , required:true},
-    image_url:{type:String , default:''},
-    image_id:{type:String , default:''},
-    status_code:{type:Number},
-    password:{type:String , required:true},
-    user_type:{type:String,required:true },
-    status:{type:Boolean , default:false},
-    public_id:{type:mongoose.SchemaTypes.ObjectId },
+    first_name: { type: String },
+    last_name: { type: String },
+    oauthID: { type: String },
+    name: { type: String },
+    email_address: { type: String, unique: true },
+    phone_number: { type: String },
+    image_url: { type: String, default: '' },
+    image_id: { type: String, default: '' },
+    status_code: { type: Number },
+    password: { type: String },
+    user_type: { type: String },
+    status: { type: Boolean, default: false },
+    public_id: { type: mongoose.SchemaTypes.ObjectId },
     created_at: { type: Date, default: Date.now },
 })
 
