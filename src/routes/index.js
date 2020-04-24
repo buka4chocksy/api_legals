e81552e132d2d20daf68cf4cffe860d5d0b83023
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-04-24 10:37:19 +0100

    added panice alert to index routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const jurisdictionRoutes = require('./jurisdictionRoutes')
 const lawyerRoutes = require('./lawyerRoutes')
 const lawFirmRoutes = require('./lawFirmRoutes')
 const clientRoutes = require('./clientRoutes');
+const pannicAlert = require('./pannicRoutes');
 const matter = require('./matterRoutes')
 module.exports = (router) => {
     router.use('/auth', authRoutes());
@@ -13,5 +14,6 @@ module.exports = (router) => {
     router.use('/lawfirm', lawFirmRoutes())
     router.use('/client', clientRoutes())
     router.use('/matter', matter())
+    router.use('/pannicAlert', pannicAlert())
     return router;
 }
