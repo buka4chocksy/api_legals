4e045d86cb173b636c1f85ebcd6f72d5679660aa
Igwe Augustine <augustusbest00@gmail.com>
2020-04-25 12:21:46 +0100

    error fix

--- src/auth/google.js
+++ src/auth/google.js
@@ -4,22 +4,34 @@ const User = require('../models/users');
 
 module.exports = new GoogleStrategy(google, async (accessToken, refreshToken, profile, done) => {
     User.findOne({ oauthID: profile.id }, (err, user) => {
-        if(err) console.log(err);
-        if(!err && user !== null) {
+        if (err) console.log(err);
+        if (!err && user !== null) {
             done(null, user);
         } else {
-            user = new User({
+            user = {
                 oauthID: profile.id,
                 name: profile.displayName,
-                image_url:profile._json.picture
-            })
-            user.save(() => {
-                if(err) console.log(err);
-                else {
-                    console.log('saving google user...')
+                image_url: profile._json.picture
+            }
+            User.create(user, (err, us) => {
+                if(err) console.log('Error ocurred while creating this user')
+                if(us) {
+                    console.log('Google User created successfully!')
                     done(null, user)
                 }
             })
+            // user = new User({
+            //     oauthID: profile.id,
+            //     name: profile.displayName,
+            //     image_url:profile._json.picture
+            // })
+            // user.save(() => {
+            //     if(err) console.log(err);
+            //     else {
+            //         console.log('saving google user...')
+            //         done(null, user)
+            //     }
+            // })
         }
     })
 })
