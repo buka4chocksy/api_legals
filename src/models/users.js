b8eac70cfaf3daf2506cf909669df24ba50886c5
Igwe Augustine <augustusbest00@gmail.com>
2020-04-29 11:15:23 +0100

    added linkedin feature

--- src/models/users.js
+++ src/models/users.js
@@ -17,4 +17,4 @@ const userSchema = new schema({
     created_at: { type: Date, default: Date.now },
 })
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('users', userSchema);
