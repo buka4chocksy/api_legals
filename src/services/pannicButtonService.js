8561c54b1d47809b4f802b4f46c3413e0cb4b51f
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-04-30 15:43:43 +0100

    modified

--- src/services/pannicButtonService.js
+++ src/services/pannicButtonService.js
@@ -1,12 +1,14 @@
 const model = require('../models/pannicButton');
-
-exports.createPannic = (data,id , usertype)=>{
+const user = require('../models/users');
+exports.createPannic = (data,id,usertype)=>{
     return new Promise((resolve , reject)=>{
         model.findOne({public_id:id}).exec((err , exists)=>{
             if(err)reject(err);
             if(exists){
                 resolve({success:false , message:'details already exists'})
             }else{
+                //while creating pannic as client front end will have to pick the usertype data from front end 
+                //but as a lawyer the usertype will come from the database
                 const details ={
                     next_of_kin:data.next_of_kin,
                     country_code:data.country_code,
@@ -14,12 +16,23 @@ exports.createPannic = (data,id , usertype)=>{
                     email:data.email,
                     relationship:data.relationship,
                     alert:data.alert,
-                    user_type:usertype,
+                    user_type:data.user_type == '' ? usertype : data.user_type ,
                     public_id:id
                 }
                 model.create(details).then(created =>{
                     if(created){
-                        resolve({success:true , message:'pannic alert details created successfully'})
+                        if(data.user_type === 'client'){
+                            user.findOneAndUpdate({public_id:id}, {user_type:data.user_type}).exec((err , result)=>{
+                                if(err)reject(err);
+                                if(result){
+                                    resolve({success:true , message:'pannic alert details created successfully'}) 
+                                }else{
+                                    resolve({success:true , message:'Error creating pannic alert'})
+                                }
+                            })
+                        }else{
+                            resolve({success:true , message:'pannic alert details created successfully'})
+                        }
                     }else{
                         resolve({success:false , message:' error encountered while creating pannic alert details'})   
                     }
