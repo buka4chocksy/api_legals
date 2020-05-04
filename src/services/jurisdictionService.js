cc15fa9a55583201fa80549deee317f707e1b89a
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-05-04 23:00:54 +0100

    modified jurisdiction service res status

--- src/services/jurisdictionService.js
+++ src/services/jurisdictionService.js
@@ -5,18 +5,18 @@ exports.create = (data) => {
     return new Promise((resolve, reject) => {
         model.findOne({ name: data.name }).then(exists => {
             if (exists) {
-                resolve({ success: false, message: 'jurisdiction already exists !!!' })
+                resolve({ success: false, message: 'jurisdiction already exists !!!', status:400})
             } else {
                 const detail = { name: data.name }
                 model.create(detail).then(created => {
                     if (created) {
-                        resolve({ success: true, message: 'jurisdiction created successfully' })
+                        resolve({ success: true, message: 'jurisdiction created successfully', status:200 })
                     } else {
-                        resolve({ success: false, message: 'could not create jurisdiction' })
+                        resolve({ success: false, message: 'could not create jurisdiction', status:400 })
                     }
-                }).catch(err => reject(err))
+                }).catch(err => reject({err: err , status:500}))
             }
-        }).catch(err => reject(err));
+        }).catch(err => reject({err: err , status:500}));
     })
 }
 
@@ -25,11 +25,11 @@ exports.update = (id, new_name) => {
     return new Promise((resolve, reject) => {
         model.findByIdAndUpdate({ _id: id }, { name: new_name }).then(updated => {
             if (updated) {
-                resolve({ success: true, message: 'jurisdiction updated successfully' })
+                resolve({ success: true, message: 'jurisdiction updated successfully' , status:200 })
             } else {
-                resolve({ success: false, message: 'Error encountered while updating jurisdiction !!!' })
+                resolve({ success: false, message: 'Error encountered while updating jurisdiction !!!', status:400 })
             }
-        }).catch(err => reject(err))
+        }).catch(err => reject({err: err , status:500}))
     })
 }
 
@@ -38,11 +38,11 @@ exports.getAll = () => {
     return new Promise((resolve, reject) => {
         model.find({}).then(data => {
             if (data) {
-                resolve({ success: true, message: 'jurisdiction', data: data })
+                resolve({ success: true, message: 'jurisdiction', data: data , status:200 })
             } else {
-                resolve({ success: false, message: 'no jurisdiction found !!!' })
+                resolve({ success: false, message: 'no jurisdiction found !!!' ,status:404})
             }
-        }).catch(err => reject(err));
+        }).catch(err => reject({err: err , status:500}));
     })
 }
 
@@ -51,11 +51,11 @@ exports.getById = (data) => {
     return new Promise((resolve, reject) => {
         model.findOne({ _id: data }).then(found => {
             if (found) {
-                resolve({ success: true, message: found })
+                resolve({ success: true, message: found , status:200 })
             } else {
-                resolve({ success: false, message: 'could not find jurisdiction !!' })
+                resolve({ success: false, message: 'could not find jurisdiction !!' ,status:404})
             }
-        }).catch(err => reject(err));
+        }).catch(err => reject({err: err , status:500}));
     })
 }
 
@@ -64,10 +64,10 @@ exports.delete = (data) => {
     return new Promise((resolve, reject) => {
         model.findOneAndRemove({ _id: data }).then(found => {
             if (found) {
-                resolve({ success: true, message: 'jurisdiction deleted' })
+                resolve({ success: true, message: 'jurisdiction deleted',status:200 })
             } else {
-                resolve({ success: false, message: 'could not delete jurisdiction !!' })
+                resolve({ success: false, message: 'could not delete jurisdiction !!', status:400 })
             }
-        }).catch(err => reject(err));
+        }).catch(err => reject({err: err , status:500}));
     })
 }
