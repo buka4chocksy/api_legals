282cd75abd366602140cafe3c9e5cb1223297ee8
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-05-04 22:59:04 +0100

    modified client res status

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -16,28 +16,28 @@ module.exports =  function clientController(){
         service
             .profilePicture(req.auth.publicId, requestDetails)
             .then(data => {
-                res.status(200).send(data);
+                res.status(data.status).send(data);
             })
             .catch(err => {
-                res.status(500).send(err);
+                res.status(err.status).send(err);
             });
     };
 
     this.updateClientProfile = (req, res) => {
         service.editClientProfile(req.auth.publicId, req.body).then(data => {
-            res.status(200).send(data);
-        }).catch(err => res.status(500).send(err))
+            res.status(data.status).send(data);
+        }).catch(err => res.status(err.status).send(err))
     }
 
     this.deleteUserAccount = (req, res) => {
         service.deleteAccount(req.auth.Id , req.auth.publicId).then(data => {
-            res.status(200).send(data);
-        }).catch(err => res.status(500).send(err))
+            res.status(data.status).send(data);
+        }).catch(err => res.status(err.status).send(err))
     }
 
     this.getClientDetails = (req , res)=>{
         service.getClientProfile(req.auth.publicId).then(data =>{
-            res.status(200).send(data);
-        }).catch(err => res.status(500).send(err))
+            res.status(data.status).send(data);
+        }).catch(err => res.status(err.status).send(err))
     }
 }
