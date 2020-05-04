e77344572344d120a6f3b8ae1b0c0b9596c62d3a
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-05-04 23:02:21 +0100

    modified practice area res status

--- src/services/practiceAreaService.js
+++ src/services/practiceAreaService.js
@@ -5,18 +5,18 @@ exports.create = (data) => {
     return new Promise((resolve, reject) => {
         model.findOne({ name: data.name }).then(exists => {
             if (exists) {
-                resolve({ success: false, message: 'pracitcearea already exists !!!' })
+                resolve({ success: false, message: 'pracitcearea already exists !!!', status:400 })
             } else {
                 const detail = { name: data.name }
                 model.create(detail).then(created => {
                     if (created) {
-                        resolve({ success: true, message: 'practicearea created successfully' })
+                        resolve({ success: true, message: 'practicearea created successfully' ,status:200})
                     } else {
-                        resolve({ success: false, message: 'could not create practicearea' })
+                        resolve({ success: false, message: 'could not create practicearea' , status:400 })
                     }
-                }).catch(err => reject(err))
+                }).catch(err => reject({err: err , status:500}))
             }
-        }).catch(err => reject(err));
+        }).catch(err => reject({err: err , status:500}));
     })
 }
 
@@ -25,11 +25,11 @@ exports.update = (id, new_name) => {
     return new Promise((resolve, reject) => {
         model.findByIdAndUpdate({ _id: id }, { name: new_name }).then(updated => {
             if (updated) {
-                resolve({ success: true, message: 'practicearea updated successfully' })
+                resolve({ success: true, message: 'practicearea updated successfully',status:200 })
             } else {
-                resolve({ success: false, message: 'Error encountered while updating practice area !!!' })
+                resolve({ success: false, message: 'Error encountered while updating practice area !!!', status:400 })
             }
-        }).catch(err => reject(err))
+        }).catch(err => reject({err: err , status:500}))
     })
 }
 
@@ -38,11 +38,11 @@ exports.getAll = () => {
     return new Promise((resolve, reject) => {
         model.find({}).then(data => {
             if (data) {
-                resolve({ success: true, message: 'practiceareas', data: data })
+                resolve({ success: true, message: 'practiceareas', data: data , status:200 })
             } else {
-                resolve({ success: false, message: 'no practicearea found !!!' })
+                resolve({ success: false, message: 'no practicearea found !!!' ,status:404})
             }
-        }).catch(err => reject(err));
+        }).catch(err => reject({err: err , status:500}));
     })
 }
 
@@ -51,11 +51,11 @@ exports.getById = (data) => {
     return new Promise((resolve, reject) => {
         model.findOne({ _id: data }).then(found => {
             if (found) {
-                resolve({ success: true, message: found })
+                resolve({ success: true, message: found , status:200})
             } else {
-                resolve({ success: false, message: 'could not find practicearea !!' })
+                resolve({ success: false, message: 'could not find practicearea !!' , status:404})
             }
-        }).catch(err => reject(err));
+        }).catch(err => reject({err: err , status:500}));
     })
 }
 
@@ -64,10 +64,10 @@ exports.delete = (data) => {
     return new Promise((resolve, reject) => {
         model.findOneAndRemove({ _id: data }).then(found => {
             if (found) {
-                resolve({ success: true, message: 'practicearea deleted' })
+                resolve({ success: true, message: 'practicearea deleted', status:200 })
             } else {
-                resolve({ success: false, message: 'could not delete practicearea !!' })
+                resolve({ success: false, message: 'could not delete practicearea !!' , status:400})
             }
-        }).catch(err => reject(err));
+        }).catch(err => reject({err: err , status:500}));
     })
 }
