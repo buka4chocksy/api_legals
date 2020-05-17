b85b1289f52e43ca4b42a791941d7344028d9681
Igwe Augustine <augustusbest00@gmail.com>
2020-05-17 19:23:39 +0100

    added joi validator

--- src/auth/google.js
+++ src/auth/google.js
@@ -9,26 +9,40 @@ module.exports = new GoogleStrategy(google, async (accessToken, refreshToken, pr
         if (!err && user !== null) {
             done(null, user);
         } else {
-            const userDetails = {
-                oauth: {
-                    oauthID: profile.id,
-                    provider: null
-                },
-                first_name: profile.displayName.split(" ")[0],
-                last_name: profile.displayName.split(" ")[1],
-                // oauthID: profile.id,
-                // name: profile.displayName,
-                image_url: profile._json.picture,
-            }
-            User.create(userDetails).then(created => {
+            const user = new User;
+            user.first_name = profile.displayName.split(" ")[0]
+            user.last_name = profile.displayName.split(" ")[1]
+            user.image_url = profile._json.picture;
+            user.oauth.oauthID = profile.id;
+            user.oauth.oauthID = profile.provider;
+            user.save().then(created => {
                 if (created) {
                     console.log('Google User created successfully!')
                     done(null, created)
                 }
+            }).catch(err => {
+                console.log('Error ocurred while creating this user')
             })
-                .catch(err => {
-                    console.log('Error ocurred while creating this user')
-                })
+            // const userDetails = {
+            //     oauth: {
+            //         oauthID: profile.id,
+            //         provider: profile.provider
+            //     },
+            //     first_name: profile.displayName.split(" ")[0],
+            //     last_name: profile.displayName.split(" ")[1],
+            //     // oauthID: profile.id,
+            //     // name: profile.displayName,
+            //     image_url: profile._json.picture,
+            // }
+            // User.create(userDetails).then(created => {
+            //     if (created) {
+            //         console.log('Google User created successfully!')
+            //         done(null, created)
+            //     }
+            // })
+            //     .catch(err => {
+            //         console.log('Error ocurred while creating this user')
+            //     })
         }
     })
 })
