f2bf4f7e4bf1c01a06374b17a22157b08b959ee3
Igwe Augustine <augustusbest00@gmail.com>
2020-05-17 22:01:54 +0100

    social auth fix

--- src/auth/linkedin.js
+++ src/auth/linkedin.js
@@ -4,6 +4,7 @@ const User = require('../models/users');
 
 module.exports = new LinkedInStrategy(linkedin, async (accessToken, refreshToken, profile, done) => {
     User.findOne({ oauthID: profile.id }, (err, user) => {
+        console.log(profile._json.emails)
         if (err) console.log(err);
         if (!err && user !== null) {
             done(null, user);
@@ -11,7 +12,7 @@ module.exports = new LinkedInStrategy(linkedin, async (accessToken, refreshToken
             const user = new User;
             user.first_name = profile.displayName.split(" ")[0]
             user.last_name = profile.displayName.split(" ")[1]
-            user.email_address = profile._json.emails[0].value
+            // user.email_address = profile._json.emails[0].value
             user.image_url = profile.photos[0].value;
             user.oauth.oauthID = profile.id;
             user.oauth.provider = profile.provider || null;
