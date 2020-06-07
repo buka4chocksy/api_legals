bb0a8b7c97c3e9ffe9223133a5c86d1b16312c6c
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-06-07 22:21:41 +0100

    added  add admin routes

--- src/routes/lawFirmRoutes.js
+++ src/routes/lawFirmRoutes.js
@@ -7,7 +7,9 @@ module.exports = function () {
     router.post('/create', middleware.authenticate, lawFirmCtrl.createLawFirm);
     router.put('/profile_picture', middleware.authenticate, multer.upload.single('profile'), lawFirmCtrl.uploadProfilePicture)
     router.get('/profile', middleware.authenticate , lawFirmCtrl.getlawfirmProfile)
+    router.post('/add_admin', middleware.authenticate , lawFirmCtrl.addAdmin)
     router.get('/lawfirms/:pagesize/:pagenumber', middleware.authenticate , lawFirmCtrl.getLawfirmList)
+    router.get('/my_lawfirms/:pagesize/:pagenumber', middleware.authenticate , lawFirmCtrl.lawyerLawFirmList)
     router.get('/sort_practice_area/:id/:pagesize/:pagenumber', middleware.authenticate , lawFirmCtrl.sortLawFirmByPracticeArea)
     router.get('/sort_location/:pagesize/:pagenumber', middleware.authenticate , lawFirmCtrl.sortLawFirmByLocation)
     router.get('/search', middleware.authenticate , lawFirmCtrl.searchLawFirm)
