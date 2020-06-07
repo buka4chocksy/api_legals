7f0c5f23175d1c659f2f76fb30c1751418ce97c4
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-06-07 22:19:34 +0100

    created lawfirm model

---
+++ src/models/firms/lawFirm.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+const lawFirmSchema = new schema({
+    name_of_firm:{type:String , required:true},
+    contact_email:{type:String , required:true},
+    image_url: { type: String, default: '' },
+    image_id: { type: String, default: '' },
+    website_url: { type: String, default: '' },
+    contact_phone_number:[{
+        phone_number:{type:String , require:true}
+    }],
+    practice_area: [{
+        practice_area_id: { type: mongoose.SchemaTypes.ObjectId, ref: 'practiceArea', autopopulate: true }
+    }],
+    location: [{
+        country_Id: { type:  mongoose.SchemaTypes.ObjectId, ref: 'jurisdiction', autopopulate: true },
+        state_Id: { type:  mongoose.SchemaTypes.ObjectId, ref: 'jurisdiction', autopopulate: true },
+        address: { type: String , require:true}
+    }],
+    lawyer_id:{ type: mongoose.SchemaTypes.ObjectId, ref: 'lawyer', autopopulate: true },
+    public_id:{ type: mongoose.SchemaTypes.ObjectId, ref: 'users', autopopulate: true },
+    created_at: { type: Date, default: Date.now },
+
+})
+module.exports = mongoose.model('lawFirm', lawFirmSchema);
