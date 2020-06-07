5ca3abcc6be73e2e41594b0c73440748163c8986
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-06-07 22:18:11 +0100

    modified

---
+++ src/models/auth/users.js
@@ -0,0 +1,29 @@
+
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+const userSchema = new schema({
+    first_name: { type: String },
+    last_name: { type: String },
+    name: { type: String },
+    email_address: { type: String, unique: true },
+    phone_number: { type: String },
+    image_url: { type: String, default: '' },
+    image_id: { type: String, default: '' },
+    status_code: { type: Number },
+    password: { type: String },
+    user_type: { type: String, lowercase: true },
+    status: { type: Boolean, default: false },
+    public_id: { type: mongoose.SchemaTypes.ObjectId },
+    created_at: { type: Date, default: Date.now },
+    token: {
+        tokenID: {type: String, default: null},
+        deviceID: {type: String, default: null}
+    },
+    oauth: {
+        provider: {type: String, enum: ['google', 'linkedin']},
+        oauthID: {type: String},
+        status: false
+    }
+})
+
+module.exports = mongoose.model('users', userSchema);
