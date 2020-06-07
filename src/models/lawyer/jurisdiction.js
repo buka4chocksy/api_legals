989721c947e9629d3d64be31fcd98ba8a96e9415
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-06-07 22:19:43 +0100

    modified

---
+++ src/models/lawyer/jurisdiction.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+const jurisdiction_schema = new schema({
+    name: { type: String, required: true }
+})
+module.exports = mongoose.model('jurisdiction', jurisdiction_schema)
