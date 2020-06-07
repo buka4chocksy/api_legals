6a95eefedf8e769399788f54741d8f637f5d0b89
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-06-07 22:19:02 +0100

    created admin model

---
+++ src/models/firms/admin.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+var AdminSchema = new schema({
+    firm : {type : mongoose.SchemaTypes.ObjectId, required : true, ref : 'firm'},
+    lawyer : {type : mongoose.SchemaTypes.ObjectId, required : true, ref : 'lawyer'},
+}, {timestamps : { currentTime : () => Math.floor(Date.now() / 1000)}})
+
+module.exports = mongoose.model('admin', AdminSchema)
+
+
+
