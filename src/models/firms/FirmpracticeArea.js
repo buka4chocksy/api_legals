e2b43532a5bf8e7d238573290cbf4b6769aee090
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-06-14 14:16:48 +0100

    moved firm practice area model

---
+++ src/models/firms/FirmpracticeArea.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+var FirmpracticeAreaSchema = new schema({
+    firm : {type : mongoose.SchemaTypes.ObjectId, required : true, ref : 'firm'},
+    practice_area: { type: mongoose.SchemaTypes.ObjectId, ref: 'practiceArea', autopopulate: true }
+}, {timestamps : { currentTime : () => Math.floor(Date.now() / 1000)}})
+
+module.exports = mongoose.model('firmpracticeArea', FirmpracticeAreaSchema)
+
+
+
