0e2d6e28131eeeb20a49f41502889ad902c923c3
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-06-14 14:18:10 +0100

    added getSinglelawfirm , addLawyerToLawfirm, getLawfirmLawyers, getLawfirmAdmin , addlocation  routes

--- src/routes/lawFirmRoutes.js
+++ src/routes/lawFirmRoutes.js
@@ -7,12 +7,17 @@ module.exports = function () {
     router.post('/create', middleware.authenticate, lawFirmCtrl.createLawFirm);
     router.put('/profile_picture', middleware.authenticate, multer.upload.single('profile'), lawFirmCtrl.uploadProfilePicture)
     router.get('/profile', middleware.authenticate , lawFirmCtrl.getlawfirmProfile)
+    router.get('/single_lawfirm', middleware.authenticate , lawFirmCtrl.getSinglelawfirm)
+    router.put('/add_lawyer_to_lawfirm', middleware.authenticate , lawFirmCtrl.addLawyerToLawfirm)
     router.post('/add_admin', middleware.authenticate , lawFirmCtrl.addAdmin)
     router.get('/lawfirms/:pagesize/:pagenumber', middleware.authenticate , lawFirmCtrl.getLawfirmList)
     router.get('/my_lawfirms/:pagesize/:pagenumber', middleware.authenticate , lawFirmCtrl.lawyerLawFirmList)
     router.get('/sort_practice_area/:id/:pagesize/:pagenumber', middleware.authenticate , lawFirmCtrl.sortLawFirmByPracticeArea)
     router.get('/sort_location/:pagesize/:pagenumber', middleware.authenticate , lawFirmCtrl.sortLawFirmByLocation)
+    router.get('/firm_lawyers', middleware.authenticate , lawFirmCtrl.getLawfirmLawyers)
+    router.get('/firm_Admin', middleware.authenticate , lawFirmCtrl.getLawfirmAdmin)
     router.get('/search', middleware.authenticate , lawFirmCtrl.searchLawFirm)
-
+    router.post('/updateLocation', middleware.authenticate, lawFirmCtrl.addlocation)
+    router.post('/addMorePracticearea', middleware.authenticate, lawFirmCtrl.addPracticeAreaTolawFirm)
     return router;
 }
