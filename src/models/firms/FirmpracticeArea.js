bcf74252ef73924ed380dbae7da8ec527d9c59d9
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-06-17 13:27:07 +0100

    modified practice area model

--- src/models/firms/FirmpracticeArea.js
+++ src/models/firms/FirmpracticeArea.js
@@ -3,7 +3,9 @@ const schema = mongoose.Schema;
 
 var FirmpracticeAreaSchema = new schema({
     firm : {type : mongoose.SchemaTypes.ObjectId, required : true, ref : 'firm'},
-    practice_area: { type: mongoose.SchemaTypes.ObjectId, ref: 'practiceArea', autopopulate: true }
+    practice_area: { type: mongoose.SchemaTypes.ObjectId, ref: 'practiceArea', autopopulate: true },
+    softDelete:{type:Boolean , default: false },
+
 }, {timestamps : { currentTime : () => Math.floor(Date.now() / 1000)}})
 
 module.exports = mongoose.model('firmpracticeArea', FirmpracticeAreaSchema)
