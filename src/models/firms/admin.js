caa20957505814308f7b44d915acd10b58876ad4
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-06-17 13:26:06 +0100

    modified admin model

--- src/models/firms/admin.js
+++ src/models/firms/admin.js
@@ -4,6 +4,7 @@ const schema = mongoose.Schema;
 var AdminSchema = new schema({
     firm : {type : mongoose.SchemaTypes.ObjectId, required : true, ref : 'firm'},
     lawyer : {type : mongoose.SchemaTypes.ObjectId, required : true, ref : 'lawyer'},
+    softDelete:{type:Boolean , default: false },
 }, {timestamps : { currentTime : () => Math.floor(Date.now() / 1000)}})
 
 module.exports = mongoose.model('admin', AdminSchema)
