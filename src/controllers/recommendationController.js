2adc6fdd162e3502ec829cd629adf90a9a7caf93
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-06-19 00:33:43 +0100

    created create recommendaton and get lawyer recommendaton methods

---
+++ src/controllers/recommendationController.js
@@ -0,0 +1,20 @@
+const service = require('../services/recommendation');
+
+module.exports = function recommendationController(){
+
+    this.create = (req,res)=>{
+        service.createRecommendation(req.auth.publicId,req.body).then(data =>{
+            res.status(data.status).send(data)
+        }).catch(err => res.status(err.status).send(err));
+    }
+
+    this.getLawyerRecommendations = (req,res)=>{
+        service.getLawyerRecommendations(req.auth.publicId).then(data =>{
+            res.status(data.status).send(data)
+        }).catch(err => res.status(err.status).send(err));
+
+    }
+
+
+
+}
