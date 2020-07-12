193fbc9660063b5261bfe7f0aecbf3b4a58e748a
Emmanuel Okoye <developeradmin@genesystechhub.com>
2020-07-13 00:17:58 +0100

    hard coded oauth callback url fixed

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,27 +1,30 @@
+const { AUTH_CALLBACK_URL } = process.env;
+
+
 module.exports = {
-    google: {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'https://lawyerapp-api.herokuapp.com/auth/google/callback',
-    },
-    googleLogin: {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'https://lawyerapp-api.herokuapp.com/auth/google/callback/login',
-    },
-    linkedin: {
-      clientID: process.env.LINKEDIN_CLIENT_ID,
-      clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
-      callbackURL: 'https://lawyerapp-api.herokuapp.com/auth/linkedin/callback',
-      scope: ['r_emailaddress', 'r_liteprofile'],
-      state: true
-    },
-    linkedinLogin: {
-      clientID: process.env.LINKEDIN_CLIENT_ID,
-      clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
-      callbackURL: 'https://lawyerapp-api.herokuapp.com/auth/linkedin/callback/login',
-      scope: ['r_emailaddress', 'r_liteprofile'],
-      state: true
-    },
-  }
+  google: {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: AUTH_CALLBACK_URL + '/auth/google/callback',
+  },
+  googleLogin: {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: AUTH_CALLBACK_URL + '/auth/google/callback/login',
+  },
+  linkedin: {
+    clientID: process.env.LINKEDIN_CLIENT_ID,
+    clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+    callbackURL: AUTH_CALLBACK_URL + '/auth/linkedin/callback',
+    scope: ['r_emailaddress', 'r_liteprofile'],
+    state: true
+  },
+  linkedinLogin: {
+    clientID: process.env.LINKEDIN_CLIENT_ID,
+    clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+    callbackURL: AUTH_CALLBACK_URL + '/auth/linkedin/callback/login',
+    scope: ['r_emailaddress', 'r_liteprofile'],
+    state: true
+  },
+};
 
