5f03719ac73f1279de0f6abfa2eb4c8d854c0f48
Emmanuel Okoye <developeradmin@genesystechhub.com>
2020-07-14 14:22:25 +0100

    auth publicId changed to public_id

--- src/controllers/recommendationController.js
+++ src/controllers/recommendationController.js
@@ -3,13 +3,13 @@ const service = require('../services/recommendationService');
 module.exports = function recommendationController(){
 
     this.create = (req,res)=>{
-        service.createRecommendation(req.auth.publicId,req.body).then(data =>{
+        service.createRecommendation(req.auth.public_id,req.body).then(data =>{
             res.status(data.status).send(data)
         }).catch(err => res.status(err.status).send(err));
     }
 
     this.getLawyerRecommendations = (req,res)=>{
-        service.getLawyerRecommendations(req.auth.publicId).then(data =>{
+        service.getLawyerRecommendations(req.auth.public_id).then(data =>{
             res.status(data.status).send(data)
         }).catch(err => res.status(err.status).send(err));
 
