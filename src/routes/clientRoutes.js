e32579a82923c9a86663c7ea3a4af3e67a689c01
Okeke Chekwube <okekechekwube1@gmail.com>
2020-07-23 16:04:43 +0100

    Updated client route

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -8,7 +8,7 @@ module.exports = function(){
     router.put('/profile_picture', middleware.authenticate, multer.upload.single('profile'), clientCtrl.uploadProfilePicture)
     router.put('/update_profile', middleware.authenticate, clientCtrl.updateClientProfile)
     router.delete('/delete_account', middleware.authenticate, clientCtrl.deleteUserAccount)
-    router.get('/client_profile', middleware.authenticate , clientCtrl.getClientDetails)
+    router.get('/profile', middleware.authenticate , clientCtrl.getClientDetails)
 
 
     return router;
