7d11f80d58c34893414094c1c2d6087cf70a613e
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-08-26 15:17:21 +0100

    moified create matter route

--- src/routes/matterRoutes.js
+++ src/routes/matterRoutes.js
@@ -4,7 +4,7 @@ const middleware = require('../middlewares/authMiddleware');
 const multer = require('../middlewares/multer');
 module.exports = function () {
     const matterCtrl = new matterController()
-    router.put('/create', middleware.authenticate, multer.upload.fields([{ name: 'image', maxCount: 1 },{ name: 'audio', maxCount: 1 }]), matterCtrl.createMatter);
+    router.put('/create', middleware.authenticate, multer.upload.fields([{ name: 'image', maxCount: 1 }]), matterCtrl.createMatter);
     router.get('/pending_open_matters/:pagesize/:pagenumber' , middleware.authenticate, matterCtrl.GetClientPendingOpenMatter);
     router.get('/pending_specified_matters/:pagesize/:pagenumber' , middleware.authenticate, matterCtrl.getPendingSpecifiedMatter);
     router.get('/sort_by_practicearea/:pagesize/:pagenumber', middleware.authenticate, matterCtrl.sortClientMatterByPracticeArea)
